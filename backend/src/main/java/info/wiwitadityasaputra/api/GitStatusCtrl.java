4ff839d74eb89f15fc5e851bb68929e7f3ece8fa
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-09-14 01:59:31 +0700

    Fix cors for each ctrl

--- backend/src/main/java/info/wiwitadityasaputra/api/GitStatusCtrl.java
+++ backend/src/main/java/info/wiwitadityasaputra/api/GitStatusCtrl.java
@@ -2,10 +2,12 @@ package info.wiwitadityasaputra.api;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api/git-status")
 public class GitStatusCtrl extends AbstractCtrl {
