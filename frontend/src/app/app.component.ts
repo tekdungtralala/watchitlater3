6fdda623066f90788c50d1922c503245c9dfdfa4
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-09-14 11:23:20 +0700

    Create simple router

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {ServerService} from "./server.service";
+import { Router } from "@angular/router";
+
+import { ServerService } from "./server.service";
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,36 @@ import {ServerService} from "./server.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'watch it later';
 
-  constructor(private ss:ServerService) {
+  constructor(private ss:ServerService, private router: Router) {
     console.log('ssd');
     this.ss.getGitStatus().subscribe(
       (response) => console.log(response)
     );
+  }
+
+  onLoadHome() {
+    console.log('onLoadHome');
+    this.router.navigate(['/']);
+  }
+
+  onLoadTop100() {
+    console.log('onLoadTop100');
+    this.router.navigate(['/top100']);
+  }
+
+  onLoadLatest() {
+    console.log('onLoadLatest');
+    this.router.navigate(['/latest']);
+  }
+
+  onLoadRegister() {
+    console.log('onLoadRegister');
+    this.router.navigate(['/register']);
+  }
+
+  onLoadLogin() {
+    console.log('onLoadLogin');
+    this.router.navigate(['/login']);
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from "@angular/http";
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ServerService } from "./server.service";
+import { HomeComponent } from './home/home.component';
+import { Top100Component } from './top100/top100.component';
+import { LatestComponent } from './latest/latest.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'top100', component: Top100Component },
+  { path: 'latest', component: LatestComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Top100Component,
+    LatestComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServerService],
   bootstrap: [AppComponent]
