0e93596ec8c0549a0e822fae5f3592417e883568
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-10-05 17:13:37 +0700

    Remove Options as allowed method

--- backend/src/main/java/info/wiwitadityasaputra/config/WebMvcConfig.java
+++ backend/src/main/java/info/wiwitadityasaputra/config/WebMvcConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("HEAD", "OPTIONS", "GET", "PUT", "POST", "DELETE", "PATCH");
+		registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 	}
 }
