f6d05546b3b3f2cb2eb354bda7e523094e3293a7
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-10-06 10:12:07 +0700

    Add active link

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, HostListener, Inject, OnInit} from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,61 +9,58 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
 
+  private mustShrink: boolean = false;
   isHeaderShrink: boolean = false;
+  currentRouter: string = '/';
 
   @HostListener('window:scroll', []) onScrollEvent() {
     const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.isHeaderShrink = number > 100 ? true: false;
+    this.isHeaderShrink = this.mustShrink || number > 100 ? true: false;
   }
 
   constructor(@Inject(DOCUMENT) private document: Document, private router: Router) {
   }
 
   ngOnInit() {
-    console.log('start');
-
-    // Smooth scrolling using jQuery easing
-    // $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    //     var target = $(this.hash);
-    //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    //     if (target.length) {
-    //       $('html, body').animate({
-    //         scrollTop: (target.offset().top - 48)
-    //       }, 1000, "easeInOutExpo");
-    //       return false;
-    //     }
-    //   }
-    // });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    // $('.js-scroll-trigger').click(function() {
-    //   $('.navbar-collapse').collapse('hide');
-    // });
+    this.router.events.subscribe(
+      (val: any) => {
+        this.isHeaderShrink = val.url !== '/';
+      }
+    );
   }
 
   onLoadHome() {
     console.log('onLoadHome');
+    this.mustShrink = false;
+    this.currentRouter = '/';
     this.router.navigate(['/']);
   }
 
   onLoadTop100() {
     console.log('onLoadTop100');
+    this.mustShrink = true;
+    this.currentRouter = '/top100';
     this.router.navigate(['/top100']);
   }
 
   onLoadLatest() {
     console.log('onLoadLatest');
+    this.mustShrink = true;
+    this.currentRouter = '/latest';
     this.router.navigate(['/latest']);
   }
 
   onLoadRegister() {
     console.log('onLoadRegister');
+    this.mustShrink = true;
+    this.currentRouter = '/register';
     this.router.navigate(['/register']);
   }
 
   onLoadLogin() {
     console.log('onLoadLogin');
+    this.mustShrink = true;
+    this.currentRouter = '/login';
     this.router.navigate(['/login']);
   }
 }
