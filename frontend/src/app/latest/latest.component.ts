d5f7051fdb98593332a328c547cbd81abd2e2577
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-10-08 21:55:29 +0700

    Show movie week

--- frontend/src/app/latest/latest.component.ts
+++ frontend/src/app/latest/latest.component.ts
@@ -5,6 +5,16 @@ import * as moment from 'moment';
 import { ServerService } from '../app-util/server.service';
 import { MovieGroupNameModel } from '../app-util/movie.model';
 
+const equals = (one: NgbDateStruct, two: NgbDateStruct) =>
+  one && two && two.year === one.year && two.month === one.month && two.day === one.day;
+
+const before = (one: NgbDateStruct, two: NgbDateStruct) =>
+  !one || !two ? false : one.year === two.year ? one.month === two.month ? one.day === two.day
+    ? false : one.day < two.day : one.month < two.month : one.year < two.year;
+
+const after = (one: NgbDateStruct, two: NgbDateStruct) =>
+  !one || !two ? false : one.year === two.year ? one.month === two.month ? one.day === two.day
+    ? false : one.day > two.day : one.month > two.month : one.year > two.year;
 
 @Component({
   selector: 'app-latest',
@@ -12,20 +22,51 @@ import { MovieGroupNameModel } from '../app-util/movie.model';
   styleUrls: ['./latest.component.css']
 })
 export class LatestComponent implements OnInit {
-  model: NgbDateStruct;
-  date: {year: number, month: number};
+  currentDate: NgbDateStruct;
+  selectedDate: NgbDateStruct;
+  fromDate: NgbDateStruct;
+  toDate: NgbDateStruct;
 
   constructor(private serverService: ServerService) {
   }
 
   ngOnInit() {
-    const m: moment.Moment = moment(new Date());
-    const currentDate = m.format('YYYY-MM-DD');
-    this.serverService.getMovieGroupName(currentDate).subscribe(
-      (value: MovieGroupNameModel) => {
-        console.log(value);
-      }
-    );
+    // const m: moment.Moment = moment(new Date());
+    // const currentDate = m.format('YYYY-MM-DD');
+    const now = new Date();
+    this.selectedDate = {
+      year: now.getFullYear(),
+      month: now.getMonth() + 1,
+      day: now.getDate()
+    };
+    this.currentDate = this.selectedDate;
   }
+
+  onDateChanged() {
+    if (after(this.currentDate, this.selectedDate) || equals(this.currentDate, this.selectedDate)) {
+      const date: string = this.selectedDate.year + '-' + this.selectedDate.month + '-' + this.selectedDate.day;
+      this.serverService.getMovieGroupName(date).subscribe(
+        (value: MovieGroupNameModel) => {
+          const fdow: string[] = value.firstDayOfWeek.split('-');
+          const ldow: string[] = value.lastDayOfWeek.split('-');
+          this.fromDate = {
+            year: +fdow[0],
+            month: +fdow[1],
+            day: +fdow[2]
+          }
+          this.toDate = {
+            year: +ldow[0],
+            month: +ldow[1],
+            day: +ldow[2]
+          }
+        }
+      );
+    }
+  }
+
+
+  isInside = date => after(date, this.fromDate) && before(date, this.toDate);
+  isFrom = date => equals(date, this.fromDate);
+  isTo = date => equals(date, this.toDate);
 
 }
