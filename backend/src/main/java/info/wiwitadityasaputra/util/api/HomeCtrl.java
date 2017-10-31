35d39b4c9e5ebaeb5ea55ba460f87514e71abf15
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2017-10-31 10:55:10 +0700

    Route all angular page to index.html

---
+++ backend/src/main/java/info/wiwitadityasaputra/util/api/HomeCtrl.java
@@ -0,0 +1,21 @@
+package info.wiwitadityasaputra.util.api;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeCtrl extends AbstractCtrl {
+
+	private Logger logger = LogManager.getLogger(HomeCtrl.class);
+
+	@RequestMapping(value = { "/", "/top100", "/latest", "/register", "/login",
+			"/dashboard" }, method = RequestMethod.GET)
+	public String getIndex(Model model) throws Exception {
+		logger.info("HomeCtrl getIndex()");
+		return "index";
+	}
+}
