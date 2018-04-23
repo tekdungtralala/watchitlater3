5349cd94ac3fc4dcaaa43e75e0858bf4d533d82f
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2018-04-23 16:57:51 +0700

    Improve MovieFavoriteTest

--- backend/src/test/java/info/wiwitadityasaputra/moviefavorite/MovieFavoriteTest.java
+++ backend/src/test/java/info/wiwitadityasaputra/moviefavorite/MovieFavoriteTest.java
@@ -1,8 +1,12 @@
 package info.wiwitadityasaputra.moviefavorite;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import info.wiwitadityasaputra.movie.Movie;
 import info.wiwitadityasaputra.test.utility.AbstractEntityTest;
+import info.wiwitadityasaputra.user.entity.User;
 
 public class MovieFavoriteTest extends AbstractEntityTest {
 
@@ -12,9 +16,18 @@ public class MovieFavoriteTest extends AbstractEntityTest {
 		mf.setId(integer);
 		mf.setFavorite(bol);
 		mf.setPosition(integer);
+		Movie m = new Movie();
+		m.setId(integer);
+		mf.setMovie(m);
+		User u = new User();
+		mf.setUser(u);
 		
 		assertInt(mf.getId());
 		assertBoolean(mf.isFavorite());
 		assertInt(mf.getPosition());
+		assertEquals(m, mf.getMovie());
+		assertInt(mf.getMovieId());
+		assertEquals(u, mf.getUser());
+		
 	}
 }
